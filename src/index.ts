29000cefe643d5eead84e9c6a8f1dfcd19a23428
Jin Chang Yeob(진창엽) <jincy713@gmail.com>
2020-12-26 17:51:18 +0900

    feat: 간단한 블럭체인 생성, 검증 함수 구현.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
     public index: number;
     public hash: string;
@@ -18,12 +20,63 @@ class Block {
         this.data = data;
         this.timestamp = timestamp;
     }
+
+    static calculateBlockHash = (index: number, previousHash: string, timestamp: number, data: string): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+    static validateStructure = (aBlock: Block): boolean => {
+        return typeof aBlock.index === "number" && typeof aBlock.hash === "string" && typeof aBlock.previousHash === "string" && typeof aBlock.timestamp === "number" && typeof aBlock.data === "string";
+    }
 }
 
 const genesisBlock: Block = new Block(0, "20201226", "", "Jin's coin.", 1608970296042);
 
-let blockchain: [Block] = [genesisBlock];
+let blockchain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockchain;
+
+const getLatestBlock = (): Block => getBlockChain()[blockchain.length - 1];
+
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+    if (!Block.validateStructure(candidateBlock)) {
+        return false;
+    }
+
+    if (previousBlock.index + 1 !== candidateBlock.index) {
+        return false;
+    }
+
+    if (previousBlock.hash !== candidateBlock.previousHash) {
+        return false;
+    }
+
+    return getHashForBlock(candidateBlock) === candidateBlock.hash;
+}
+
+const addBlock = (candidateBlock: Block): void => {
+    if(isBlockValid(candidateBlock, getLatestBlock())) {
+        blockchain.push(candidateBlock);
+    }
+}
+
+const createNewBlock = (data: string): Block => {
+    const previousBlock: Block = getLatestBlock();
+    const newIndex: number = previousBlock.index + 1;
+    const newTimestamp: number = getNewTimeStamp();
+    const newHash: string = Block.calculateBlockHash(newIndex, previousBlock.hash, newTimestamp, data);
+    const newBlock: Block = new Block(newIndex, newHash, previousBlock.hash, data, newTimestamp);
+
+    addBlock(newBlock)
+    return newBlock;
+}
+
+const getHashForBlock = (aBlock: Block): string => Block.calculateBlockHash(aBlock.index, aBlock.previousHash, aBlock.timestamp, aBlock.data);
+
+createNewBlock("second Block");
+createNewBlock("third Block");
+createNewBlock("fourth Block");
 
 console.log(blockchain);
+
 
 export {};
