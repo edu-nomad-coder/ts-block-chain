0a81673b3fbf0d742d97afbe02a782fe5816d456
Jin Chang Yeob(진창엽) <jincy713@gmail.com>
2020-12-26 17:01:12 +0900

    feat: 인터페이스 생성, 함수 파라미터 객체로 변경.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
-const sayHi = (name: string, age: number, gender: string = 'male'): string => {
+interface Human {
+    name: string;
+    age: number;
+    gender: string;
+}
+
+const person = {
+    name: "Jin",
+    age: 26,
+    gender: "male",
+};
+
+const sayHi = (person: Human): string => {
+    const {name, age, gender} = person;
     return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 }
 
-const result = sayHi("Jin", 26, "male");
+const result = sayHi(person);
 console.log(result);
 
 export {};
