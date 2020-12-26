b66c91e959c2aefb18bab40e8225871c4b1725f3
Jin Chang Yeob(진창엽) <jincy713@gmail.com>
2020-12-26 14:30:39 +0900

    feat: 첫 타입스크립트 함수 만들기

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Jin",
+    age = 26,
+    gender = 'male';
+
+const sayHi = (name, age, gender = 'male') => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+}
+
+sayHi(name, age);
+
+export {};
