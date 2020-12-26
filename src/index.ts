b72336744734b49bbea0d3a3c75b5a5286b388d8
Jin Chang Yeob(진창엽) <jincy713@gmail.com>
2020-12-26 17:06:39 +0900

    feat: 클래스 생성.

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-interface Human {
-    name: string;
-    age: number;
-    gender: string;
+class Human {
+    public name: string;
+    public age: number;
+    public gender: string;
+
+    constructor(name: string, age:number, gender: string) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
 }
 
-const person = {
-    name: "Jin",
-    age: 26,
-    gender: "male",
-};
+const jin  = new Human("Jin", 26, "male");
 
 const sayHi = (person: Human): string => {
     const {name, age, gender} = person;
     return `Hello ${name}, you are ${age}, you are a ${gender}!`;
 }
 
-const result = sayHi(person);
+const result = sayHi(jin);
 console.log(result);
 
 export {};
